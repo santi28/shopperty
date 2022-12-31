100bd62a5f0a0913ea75e4839d5a2c69e13a388a
SantiDN <santidenicolas@gmail.com>
2022-12-31 19:29:37 -0300

    adding db configuration and reading env config, updated readme

--- README.md
+++ README.md
@@ -4,7 +4,18 @@
 
 <h1 align="center">Shopperty API</h1>
 
+## Launch project in development mode
+
 ```bash
+# Install project dependencies
+$ pnpm install
+
+# Clone .env.example to .env
+$ cp .env.example .env
+
 # Launch database form docker-compose
 $ docker-compose up -d
+
+# Launch project in development mode
+$ pnpm start:dev
 ```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
