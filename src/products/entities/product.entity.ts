16ddc4a2b9f54a7f9d3fef9dfb28e576439d59b5
SantiDN <santidenicolas@gmail.com>
2023-02-02 22:04:33 -0300

    cambios

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,10 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ProductImage } from '.';
 
 @Entity()
 export class Product {
@@ -35,7 +37,10 @@ export class Product {
   @Column('text', { array: true, default: [] })
   tags: string[];
 
-  // images
+  @OneToMany(() => ProductImage, (productImage) => productImage.product, {
+    cascade: true,
+  })
+  images?: ProductImage;
 
   @BeforeInsert()
   slugify() {
